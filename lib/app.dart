bed95a236b63417918c52437c9ecac16f629863e
John Hwang <johnhwang@college.harvard.edu>
2024-07-29 13:59:48 -0700

    update pubspec

--- lib/app.dart
+++ lib/app.dart
@@ -43,7 +43,7 @@ class AppState extends State<App> with Nav, WidgetsBindingObserver {
           localizationsDelegates: context.localizationDelegates,
           supportedLocales: context.supportedLocales,
           locale: context.locale,
-          title: 'Image Finder',
+          title: 'PlanZ', //"image finder?"
           theme: context.themeType.themeData,
           home: const MainScreen(),
         );
@@ -64,7 +64,7 @@ class AppState extends State<App> with Nav, WidgetsBindingObserver {
         break;
       case AppLifecycleState.detached:
         break;
-      case AppLifecycleState.hidden: //Flutter 3.13 이하 버전을 쓰신다면 해당 라인을 삭제해주세요.
+      case AppLifecycleState.hidden:
         break;
     }
     super.didChangeAppLifecycleState(state);
